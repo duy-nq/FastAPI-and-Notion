6725cbbe74f69375628e8851351484a1eebc4465
Nguyen Duy <146707420+duy-nq@users.noreply.github.com>
2024-12-11 05:07:02 +0700

    feat: remove unused properties from ScientificInfo model and add new DetailedInfo model

--- app/models/notion/scientific_info.py
+++ app/models/notion/scientific_info.py
@@ -1,9 +1,6 @@
 from pydantic import BaseModel, Field
 
-from app.models.properties.checkbox import Checkbox
-from app.models.properties.number import Number
 from app.models.properties.rich_text import RichTextProperty
-from app.models.properties.title import Title
 
 class ScientificInfo(BaseModel):
     scientific_name: RichTextProperty = Field(..., alias='Tên khoa học')
